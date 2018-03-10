7de431ab1453892843a41bc3df46d2366811a98d
semhar <semaria.eyzac@gmal.com>
2018-03-09 19:55:17 -0800

    add more codes to business and user interfaces, update html and css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 //business logic
 var pingPong = function(enterNumber){
-    alert(numberArray);
+  var resultArray = [];
+  for (var i = 1; i <= enterNumber; i++){
+    if(divisibleNumber(i, 15)){
+      resultArray.push("ping pong");
+    }else if (divisibleNumber(i, 5)){
+      resultArray.push("pong");
+    }else if (divisibleNumber(i, 3)){
+      resultArray.push("ping");
+    }else {
+      resultArray.push(i);
+    }
+  }
+    //alert(numberArray);
+  alert(resultArray);
+}
 
+var divisibleNumber = function (enterNumber, value){
+  if (enterNumber % value === 0){
+    return true;
+  }
 }
 
 
@@ -12,9 +30,22 @@ var pingPong = function(enterNumber){
 $(document).ready(function(){
   $("form#num").submit(function(event){
     event.preventDefault();
-    var numberArray = []
+    //var numberArray = []
     var enterNumber = parseInt($("input#number").val());
     var result = pingPong(enterNumber);
-    $("#result").text(result);
+    $(".error").text("");
+    $("ul").empty();
+    $(pingPong(input)).forEach(function (value){
+      if (value === "ping"){
+        $("ul").append("<li class='ping shadow'>" + value + "</li>");
+      }else if (value === "pong"){
+        $("ul").append("<li class='pong shadow'>" + value + "</li>");
+      }else if (value === "ping pong"){
+        $("ul").append("<li class='pingPong shadow'>" + value + "</li>");
+      // change it into elseonly !}else{$("ul").append("<li>" + value + "</li>");
+    }
+    });
+
+
   });
 });
