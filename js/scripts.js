a281a5ff169c7b1c8c9b4efb8b391853beba6309
semhar <semaria.eyzac@gmal.com>
2018-03-09 17:58:59 -0800

    add new js features to the business interface and html features

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
+//business logic
+var pingPong = function(enterNumber){
+    alert(numberArray);
+
+}
+
+
+
+
+
+//user interface logic
 $(document).ready(function(){
   $("form#num").submit(function(event){
     event.preventDefault();
+    var numberArray = []
     var enterNumber = parseInt($("input#number").val());
-    var result = pingPong(enterName);
+    var result = pingPong(enterNumber);
     $("#result").text(result);
   });
 });
