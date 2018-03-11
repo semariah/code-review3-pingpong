80a379224742a5eb8917b35bd0afcd3b7517ce17
semhar <semaria.eyzac@gmal.com>
2018-03-10 20:06:45 -0800

    add more userinterface codes in js and update with more html elemnts

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,39 @@
 //business logic
-var //pingPong// = function(enterNumber){
+var pingPong = function(numberTo){
   var resultArray = [];
-  for (var i = 1; i <= enterNumber; i++){
-    if(divisibleNumber(i, 15)){
+  for (var i = 1; i <= numberTo; i++){
+    var index = resultArray.indexOf(i);
+    if(i % 15 === 0){
+      resultArray.slice(index, 0);
       resultArray.push("ping pong");
-    }else if (divisibleNumber(i, 5)){
+    }else if (i % 5 === 0){
+      resultArray.slice(index, 0);
       resultArray.push("pong");
-    }else if (divisibleNumber(i, 3)){
+    }else if (i % 3 === 0){
+      resultArray.slice(index, 0);
       resultArray.push("ping");
     }else {
       resultArray.push(i);
     }
   }
-    //alert(numberArray);
-  alert(resultArray);
-}
 
-var divisibleNumber = function (enterNumber, value){
-  if (enterNumber % value === 0){
-    return true;
-  }
-}
-
-
-
+    //alert(resultArray);
+    console.log(resultArray);
+  //return resultArray;
+};
 
 
 //user interface logic
 $(document).ready(function(){
   $("form#num").submit(function(event){
     event.preventDefault();
-    //var numberArray = []
-    var enterNumber = parseInt($("input#number").val());
-    var result = pingPong(enterNumber);
+    $(".result").empty();
+    var numberTo = parseInt($("input#number").val());
+    var result = pingPong(numberTo);
     //$(".error").text("");
-    $("ul").empty();
-    $(pingPong(input)).forEach(function (value){
-      if (value === "ping"){
-        $("ul").append("<li class='ping shadow'>" + value + "</li>");
-      }else if (value === "pong"){
-        $("ul").append("<li class='pong shadow'>" + value + "</li>");
-      }else if (value === "ping pong"){
-        $("ul").append("<li class='pingPong shadow'>" + value + "</li>");
-      // change it into elseonly !}else{$("ul").append("<li>" + value + "</li>");
-    }
+
+    result.forEach(function(value){
+      $(".result").append('<li>' + value + '</li>');
     });
-
-
   });
 });
